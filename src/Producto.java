5ea247e2e8d4f47facbec271828ae284df4433c4
Christian272004 <165886314+Christian272004@users.noreply.github.com>
2024-04-14 19:44:19 +0200

    Funcion de textil y electronica creadas

--- src/Producto.java
+++ src/Producto.java
@@ -3,22 +3,47 @@ import java.util.Scanner;
 public class Producto {
     static Scanner scan = new Scanner(System.in);
     public static void Alimentacio(){
-        String NomProducte;
+        String NomProducte,CodiBarras,DataCaducitat;
         int Preu;
         System.out.println("Afegir aliment");
-        System.out.print("Nom del producte: ");
+        System.out.print("Nom producte: ");
         NomProducte = scan.nextLine();
         System.out.print("Preu: ");
         Preu = scan.nextInt();
-
-
+        System.out.print("Codi de barres: ");
+        CodiBarras = scan.next();
+        System.out.print("Data de caducitat (dd/mm/aaaa): ");
+        DataCaducitat = scan.next();
+        scan.nextLine();
 
     }
 
     public static void Textil(){
-
+        String NomProducte,Composisico,CodiBarras;
+        int Preu;
+        System.out.println("Afegir tèxtil");
+        System.out.print("Nom producte: ");
+        NomProducte = scan.nextLine();
+        System.out.print("Preu: ");
+        Preu = scan.nextInt();
+        System.out.println("Composició: ");
+        Composisico = scan.next();
+        System.out.print("Codi de barres: ");
+        CodiBarras = scan.next();
+        scan.nextLine();
     }
     public static void Electronica(){
-
+        String NomProducte,Garantia,CodiBarras;
+        int Preu;
+        System.out.println("Afegir electrònica");
+        System.out.print("Nom producte: ");
+        NomProducte = scan.nextLine();
+        System.out.print("Preu: ");
+        Preu = scan.nextInt();
+        System.out.print("Garantia (dies): ");
+        Garantia = scan.next();
+        System.out.print("Codi de barres: ");
+        CodiBarras = scan.next();
+        scan.nextLine();
     }
 }
